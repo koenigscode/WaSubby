2907c56d369bed6929e99ff540df75365b98eb04
Michael Koenig <koenigmichael2002@gmail.com>
2023-09-07 13:06:40 +0200

    Edit server eslint rules #7

--- server/routes/v1/languages.js
+++ server/routes/v1/languages.js
@@ -1,4 +1,4 @@
-const router = require("express").Router()
+const router = require("express").Router();
 
 /**
  * Get all languages
@@ -8,18 +8,18 @@ const router = require("express").Router()
  * sort: "asc" or "desc"
  */
 router.get("/", function (req, res) {
-  res.status(501).send("TODO:")
-})
+    res.status(501).send("TODO:");
+});
 
 router.get("/:code", function (req, res) {
-  res.status(501).send("TODO:")
-})
+    res.status(501).send("TODO:");
+});
 
 /**
  * Update language display name (e.g. "English" to "English (US)")
  */
 router.put("/:code", function (req, res) {
-  res.status(501).send("TODO:")
-})
+    res.status(501).send("TODO:");
+});
 
-module.exports = router
+module.exports = router;
--- server/routes/v1/subtitles.js
+++ server/routes/v1/subtitles.js
@@ -1,11 +1,11 @@
-const router = require("express").Router()
+const router = require("express").Router();
 
 router.patch("/:id", function (req, res) {
-  res.status(501).send("TODO:")
-})
+    res.status(501).send("TODO:");
+});
 
 router.delete("/", function (req, res) {
-  res.status(501).send("TODO:")
-})
+    res.status(501).send("TODO:");
+});
 
-module.exports = router
+module.exports = router;
