9ba8b16d6cbf577e836913c992f846948cde19e3
Michael Koenig <koenigmichael2002@gmail.com>
2023-09-10 15:54:58 +0200

    Improve WhisperJob #9
    - delete /tmp folder after renaming srt files
    - added method jsdoc
    - refactored code into _getWhisperArgs()

--- server/services/WhisperJob.js
+++ server/services/WhisperJob.js
@@ -2,43 +2,42 @@ const spawn = require("await-spawn");
 const path = require("path");
 const fs = require("fs");
 
+const dataDir = path.join(path.resolve(__dirname, ".."), "data");
+
 /**
  * This class is responsible for transcribing and translating a locally saved media file
  * using the whisper cli ("whisper-ctranslate2" by default).
  *
  * Since there are no proper node bindings for whisper, we use the cli instead.
- * 
+ *
  * If the source language is English, the media is only transcribed,
  * otherwise the media is both transcribed and translated (to English).
  */
 class WhisperJob {
-
+    /**
+   * Creates a new whisper job.
+   * @param {string} filePath path to media file
+   * @param {number} mediaId id of media file
+   */
     constructor(filePath, mediaId) {
         this.filePath = filePath;
         this.mediaId = `${mediaId}`;
     }
 
+    /**
+   * Executes the whisper cli with the given media file, await both transcription and (if done) translation.
+   * 
+   * @returns {Promise<{transcribedLanguage: string, translated: boolean}>} object containing the transcribed language and whether the media was translated
+   */
     async execute() {
         const whisperCommand = process.env.whisperCommand || "whisper-ctranslate2";
-        const whisperModel = process.env.whisperModel || "large-v2";
-        const outputDir = 
-            path.join(
-                path.resolve(__dirname, ".."), 
-                "data");
 
-        const whisperArgs = [
-            "--output_format", "srt",
-            "--model", whisperModel,
-            "--vad_filter", "True",
-            "--condition_on_previous_text", "False",
-            this.filePath];
+        const transcriptionJob = await spawn(
+            whisperCommand,
+            this._getWhisperArgs(false)
+        );
 
-        const transcriptionJob = await spawn(whisperCommand, [
-            "--output_dir",
-            path.join(outputDir, this.mediaId, "tmp"),
-            ...whisperArgs]);
-
-        const regexPattern = /Detected language '([^']+)'/mg;
+        const regexPattern = /Detected language '([^']+)'/gm;
         const match = regexPattern.exec(transcriptionJob.toString());
 
         if (match) {
@@ -46,38 +45,90 @@ class WhisperJob {
 
             // move and rename file to [language].srt
             fs.renameSync(
-                path.join(outputDir, this.mediaId, "tmp", 
-                    path.parse(path.basename(this.filePath)).name + ".srt"),
-                path.join(outputDir, this.mediaId, `${this.language}.srt`));
+                path.join(
+                    dataDir,
+                    this.mediaId,
+                    "tmp",
+                    path.parse(path.basename(this.filePath)).name + ".srt"
+                ),
+                path.join(dataDir, this.mediaId, `${this.language}.srt`)
+            );
         } else {
             // if no language was detected, remove the tmp folder and throw an error
-            fs.rmSync(path.join(outputDir, this.mediaId, "tmp"), { recursive: true, force: true });
+            fs.rmSync(path.join(dataDir, this.mediaId, "tmp"), {
+                recursive: true,
+                force: true,
+            });
             throw new Error("Could not detect language");
-        } 
-       
-        // no translation to english needed when the source file is already in english
-        if(this.language == "English")
-            return;
+        }
 
-        await spawn(whisperCommand, [
-            "--output_dir", path.join(outputDir, this.mediaId, "tmp"),
-            "--task", "translate",
-            ...whisperArgs]);
+        // no translation to english needed when the source file is already in english
+        if (this.language == "English") {
+            fs.rmSync(path.join(dataDir, this.mediaId, "tmp"), {
+                recursive: true,
+                force: true,
+            });
+            // TODO: delete source file
+            return;
+        }
+
+        await spawn(whisperCommand, this._getWhisperArgs(true));
 
         fs.renameSync(
-            path.join(outputDir, this.mediaId, "tmp", 
-                path.parse(path.basename(this.filePath)).name + ".srt"),
-            path.join(outputDir, this.mediaId, "English.srt"));
+            path.join(
+                dataDir,
+                this.mediaId,
+                "tmp",
+                path.parse(path.basename(this.filePath)).name + ".srt"
+            ),
+            path.join(dataDir, this.mediaId, "English.srt")
+        );
+
+        fs.rmSync(path.join(dataDir, this.mediaId, "tmp"), {
+            recursive: true,
+            force: true,
+        });
+        // TODO: delete source file
+
+        return {
+            transcribedLanguage: this.language,
+            translated: true,
+        };
     }
 
+    /**
+     * Internal helper to get the whisper cli arguments for the given media file.
+   *
+   * @param {boolean} translate whether to translate the media file to english
+   */
+    _getWhisperArgs(translate) {
+        const whisperModel = process.env.whisperModel || "large-v2";
+
+        let args = [
+            "--output_format",
+            "srt",
+            "--model",
+            whisperModel,
+            "--vad_filter",
+            "True",
+            "--condition_on_previous_text",
+            "False",
+            "--output_dir",
+            path.join(dataDir, this.mediaId, "tmp"),
+            this.filePath,
+        ];
+        if (translate === true) {
+            args = ["--task", "translate", ...args];
+        }
+        return args;
+    }
+
+    /**
+   * Returns the (auto-detected) source language of the media file.
+   */
     getLanguage() {
         return this.language;
     }
-
-    deleteFile() {
-
-    }
-
 }
 
 module.exports = WhisperJob;
--- server/services/whisper_playground.js
+++ server/services/whisper_playground.js
@@ -3,7 +3,6 @@ const WhisperJob = require("./WhisperJob.js");
 
 (async function() {
     const job = new WhisperJob("/Users/koenig/code/group-22-web/server/tmp/media/test.ogg", 1);
-    await job.execute();
-    console.log("Language: " + job.getLanguage());
+    console.log(await job.execute());
 })();
 
