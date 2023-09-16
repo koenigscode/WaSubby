bd9dbe5662c33e9a4136a8f0451b3d288e29f28b
Michael Koenig <koenigmichael2002@gmail.com>
2023-09-16 14:01:04 +0300

    Insert initial data #11 - Drop database - Insert languages into DB - Insert test user

--- server/tests/initialTestData.js
+++ server/tests/initialTestData.js
@@ -7,31 +7,49 @@ const languageSchema = require("../schemas/languages");
 const mediaSchema = require("../schemas/media");
 const subtitleSchema = require("../schemas/subtitles");
 
-// Connect to MongoDB
-mongoose.connect(mongoURI).then(function() {
+async function insertData() {
+    await mongoose.connect(mongoURI);
     console.log(`Connected to MongoDB with URI: ${mongoURI}`);
-    mongoose.connection.dropDatabase();
-})
-    .catch(function (err) {
-        if (err) {
-            console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
-            console.error(err.stack);
-            process.exit(1);
-        }
-    }).then(function() {
-        //Compiling userSchema into a model and creating the user instances
-        const User = mongoose.model("User", userSchema);
-        const admin = new User({ email: "example@example.com", password: "12345678", theme: "dark" });
-     
-        //Compiling languageSchema into a model and creating the language instances
-        const Language = mongoose.model("Language", languageSchema);
-        const English = new Language({ code: "EN", name: "English" });
-        const Swedish = new Language({ code: "SE", name: "Swedish" });
-        const Korean = new Language({ code: "KO", name: "Korean" });
-        const German = new Language({ code: "DE", name: "German" });
-        const Russian = new Language({ code: "RU", name: "Russian" });
 
-        const Media = mongoose.model("Media", mediaSchema);
-        const Subtitle = mongoose.model("Subtitle", subtitleSchema);
+    await mongoose.connection.dropDatabase();
+    console.log("dropped DB");
 
-    });
+    const User = mongoose.model("User", userSchema);
+    const Language = mongoose.model("Language", languageSchema);
+    const Media = mongoose.model("Media", mediaSchema);
+    const Subtitle = mongoose.model("Subtitle", subtitleSchema);
+
+    await new Language({ code: "en", name: "English" }).save();
+    await new Language({ code: "se", name: "Swedish" }).save();
+    await new Language({ code: "ko", name: "Korean" }).save();
+    await new Language({ code: "de", name: "German" }).save();
+    await new Language({ code: "ru", name: "Russian" }).save();
+    console.log("inserted languages");
+    
+    // TODO: hash password
+    await new User({ email: "example@example.com", password: "12345678", theme: "dark" }).save();
+    console.log("inserted test user");
+    await mongoose.disconnect();
+}
+
+(async function() {
+
+    try {
+        await insertData();
+    }
+    catch(err) {
+        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
+        console.error(err.stack);
+        process.exit(1);
+    }
+})();
+
+
+// mongoose.connect(mongoURI).then(async function() {
+
+// })
+//     .catch(function (err) {
+//         if (err) {
+//            
+//         }
+//     });
