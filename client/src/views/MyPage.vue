dd16a4054f5170f673334a214e0ef2b25a47fcab
David Hong <davidbor@net.chalmers.se>
2023-10-04 00:52:20 +0200

    Modify landing page and second page Modified the landing page Added examples and buttons to the second page #14

---
+++ client/src/views/MyPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="login">
+            <div class="main">
+                <form>
+                    <label>My Page</label>
+                    <input type="text" placeholder="Enter E-Mail" />
+                    <input type="password" v-model="password" placeholder="Enter password" />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
