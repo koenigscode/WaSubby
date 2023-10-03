2ebd03847ee1303ed11cfa49d2e2f7d97ac54fb2
Michael Koenig <koenigmichael2002@gmail.com>
2023-10-03 22:22:47 +0200

    Get audio player to work #17

--- client/src/subtitle-fix.js
+++ client/src/subtitle-fix.js
@@ -8,27 +8,31 @@
  * because I couldn't load the CSS from index.html at runtime.
 */
 function fixSubs(maxCueLines = 2) {
-  const video = document.querySelector('#video')
   if (window.chrome) {
     const sheet = document.querySelector('#videoStyle').sheet
     const rule = Array.prototype.find.call(
       sheet.rules,
       (r) => r.selectorText === 'video::-webkit-media-text-track-display'
     )
-    const observer = new ResizeObserver((entries) => {
-      console.log('updated subtitle fix')
-      const height = entries[0].contentRect.height
-      const controlsHeight = 72
-      const maxCueLines = 2
-      rule.style.setProperty(
-        'top',
+
+    const elementsToFix = [document.querySelector('#video'), document.querySelector('#audio')]
+
+    for (const element of elementsToFix) {
+      const observer = new ResizeObserver((entries) => {
+        console.log('updated subtitle fix')
+        const height = entries[0].contentRect.height
+        const controlsHeight = 72
+        const maxCueLines = 2
+        rule.style.setProperty(
+          'top',
             `calc(${Math.floor(
               (1 - controlsHeight / height) * 100
             )}% - ${maxCueLines}em)`,
             'important'
-      )
-    })
-    observer.observe(video)
+        )
+      })
+      observer.observe(element)
+    }
   }
 }
 export default fixSubs
--- client/src/views/Video.vue
+++ client/src/views/Video.vue
@@ -14,18 +14,16 @@
                     Depending on the size of the media, this might take several minutes</p>
             </div>
 
-            <audio v-show="media && mediaType === 'audio' && !processing" controls ref="audioplayer">
+            <!-- This has to be a video tag, otherwise we can't display subtitles -->
+            <video id="audio" v-show="media && mediaType === 'audio' && !processing" controls ref="audioplayer" crossorigin="anonymous" preload="metadata">
                 <track kind="captions" v-for="subtitle in subtitles" :key="subtitle.id" :src="subtitle.path"
                     :srclang="subtitle.language.code" :label="subtitle.language.name">
-                <!-- <track kind="captions" src="TODO:" srclang="en" label="English">
-                <track kind="captions" src="TODO:" srclang="de" label="German"> -->
-            </audio>
+                </video>
+
             <video id="video" v-show="media && mediaType === 'video' && !processing" controls ref="videoplayer"
                 height="600px" crossorigin="anonymous" preload="metadata">
                 <track kind="captions" v-for="subtitle in subtitles" :key="subtitle.id" :src="subtitle.path"
                     :srclang="subtitle.language.code" :label="subtitle.language.name">
-                <!-- <track kind="captions" src="English.vtt" srclang="en" label="English">
-                <track kind="captions" src="German.vtt" srclang="de" label="German"> -->
             </video>
         </div>
     </div>
@@ -36,7 +34,6 @@ import fixSubs from '@/subtitle-fix'
 export default {
   data: () => ({ mediaHash: null, media: null, mediaType: null, subtitles: [], processing: false }),
   async mounted() {
-    await this.$nextTick() // wait for DOM to render
     fixSubs()
   },
   updated: function () {
@@ -62,6 +59,8 @@ export default {
         return
       }
 
+      console.log('detected mediaType: ' + this.mediaType)
+
       const formData = new FormData()
       formData.append('media', this.media)
       const headers = { 'Content-Type': 'multipart/form-data' }
@@ -77,22 +76,23 @@ export default {
       reader.addEventListener('load', () => {
         if (this.mediaType === 'video') { this.$refs.videoplayer.src = reader.result }
         if (this.mediaType === 'audio') { this.$refs.audioplayer.src = reader.result }
-
-        console.log('detected mediaType: ' + this.mediaType)
       })
       this.loadSubtitles()
     },
     showAllSubs() {
+      for (const textTrack of this.$refs.audioplayer.textTracks) {
+        textTrack.mode = 'showing'
+      }
       for (const textTrack of this.$refs.videoplayer.textTracks) {
         textTrack.mode = 'showing'
       }
     },
     async loadSubtitles() {
-      let res
-      while (this.processing) {
+      let waiting = true
+      while (waiting) {
         try {
-          res = await this.$httpClient.get(`/v1/medias/${this.mediaHash}`)
-          this.processing = res.data.processing
+          const res = await this.$httpClient.get(`/v1/medias/${this.mediaHash}`)
+          waiting = res.data.processing
         } catch (err) {
           this.$bvToast.toast(err.message, {
             title: 'Error',
@@ -104,12 +104,8 @@ export default {
         await new Promise(resolve => setTimeout(resolve, 2000))
       }
 
-      // wait before checking again
-      await new Promise(resolve => setTimeout(resolve, 2000))
-      this.processing = false
-
       // get subtitles URLs
-      res = await this.$httpClient.get(`/v1/medias/${this.mediaHash}/subtitles`)
+      const res = await this.$httpClient.get(`/v1/medias/${this.mediaHash}/subtitles`)
       const subs = res.data
       for (const sub of subs) {
         sub.path = `${process.env.VUE_APP_API_ENDPOINT}${sub.path}`
@@ -117,6 +113,8 @@ export default {
       this.subtitles = subs
       //   this.$forceUpdate()
       fixSubs(this.subtitles.length)
+      this.processing = false
+      console.log('processing finished')
     }
   }
 }
